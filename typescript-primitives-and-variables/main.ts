ff4eb8824cf41ad3b9b0c46b8d695904fc2271dd
Skylar Shaffer <lfz@skylarshaffer.com>
2024-03-25 18:36:25 +0000

    Completed typescript-primitives-and-variables exercise.

---
+++ typescript-primitives-and-variables/main.ts
@@ -0,0 +1,37 @@
+// self
+const fullName: string = 'Skylar Shaffer';
+const isCool: boolean = true;
+const totalPets: number = 0;
+
+console.log(
+  'fullName: ' +
+    fullName +
+    '\n' +
+    'isCool: ' +
+    isCool +
+    '\n' +
+    'totalPets: ' +
+    totalPets
+);
+console.log(
+  'typeof fullName: ' +
+    typeof fullName +
+    '\n' +
+    'typeof isCool: ' +
+    typeof isCool +
+    '\n' +
+    'totalPets: ' +
+    typeof totalPets
+);
+
+// empty
+const empty: null = null;
+
+console.log('empty: ' + empty);
+console.log('typeof empty: ' + typeof empty);
+
+// nothing
+let nothing: undefined;
+
+console.log('nothing: ' + nothing);
+console.log('typeof nothing: ' + typeof nothing);
