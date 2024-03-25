ae2c6778976a775004da73d903aaca3409852e67
Skylar Shaffer <lfz@skylarshaffer.com>
2024-03-25 23:19:59 +0000

    Completed typescript-template-literals exercise.

---
+++ typescript-template-literals/main.ts
@@ -0,0 +1,22 @@
+const number: number = 9200;
+const street: string = 'Irvine Center Drive';
+const address: string = `${number} ${street}`;
+
+console.log('LearningFuze:', address);
+
+const firstName: string = 'Uzair';
+const lastName: string = 'Ashraf';
+const age: number = 25;
+
+const bio: string = `My name is ${firstName} ${lastName} and I am ${age} years old.`;
+
+console.log('Bio:', bio);
+
+const fruit: string = 'apple';
+const item: string = 'pen';
+
+const applePen: string = `${fruit}${item}`;
+
+console.log('I have a', item);
+console.log('I have an', fruit);
+console.log(applePen);
---
+++ typescript-template-literals/typescript-template-literals-quiz-notes.md
@@ -0,0 +1,37 @@
+# typescript-template-literals-quiz-notes
+
+## Quiz Questions
+
+Answer the following questions in the provided markdown file before turning in this exercise:
+
+- What is the syntax for writing a template literal?
+
+- What is "string interpolation"?
+
+## Notes
+
+All student notes should be written here.
+
+How to write `Code Examples` in markdown
+
+for JS:
+
+```javascript
+const data = 'Howdy';
+```
+
+for HTML:
+
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+
+```css
+div {
+  width: 100%;
+}
+```
