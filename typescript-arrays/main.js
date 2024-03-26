67d196a568c434e5278acd30fd4f1e3b132274c6
Skylar Shaffer <lfz@skylarshaffer.com>
2024-03-26 19:07:38 +0000

    Completed typescript-arrays exercise.

--- typescript-arrays/main.js
+++ typescript-arrays/main.js
@@ -1,9 +1,9 @@
 'use strict';
 //  colors
 const colors = ['red', 'white', 'blue'];
-console.log('colors[0]', colors[0]);
-console.log('colors[1]', colors[1]);
-console.log('colors[2]', colors[2]);
+console.log('colors[0]:', colors[0]);
+console.log('colors[1]:', colors[1]);
+console.log('colors[2]:', colors[2]);
 //  America
 console.log(
   'America is ' + colors[0] + ', ' + colors[1] + ', and ' + colors[2] + '.'
