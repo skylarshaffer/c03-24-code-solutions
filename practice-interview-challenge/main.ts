b3024105abbb5132663520cb28c3c85ac92d68d2
Skylar Shaffer <lfz@skylarshaffer.com>
2024-04-22 17:05:20 +0000

    Improved practice-interview-challenge.

--- practice-interview-challenge/main.ts
+++ practice-interview-challenge/main.ts
@@ -12,46 +12,26 @@ function getTotal(start: number, end: number): number {
   return getTotalResult;
 }
 
-function getOdds(start: number, end: number): number[] {
-  const getOddsResult = [];
-  for (let i = start; i <= end; i++) {
-    if (i % 2 !== 0) {
-      getOddsResult.push(i);
-    }
-  }
-  return getOddsResult;
-}
-
-function getEvens(start: number, end: number): number[] {
-  const getEvensResult = [];
-  for (let i = start; i <= end; i++) {
-    if (i % 2 === 0) {
-      getEvensResult.push(i);
-    }
-  }
-  return getEvensResult;
-}
-
-function getRange(start: number, end: number): number[] {
-  const getRangeResult = [];
-  for (let i = start; i <= end; i++) {
-    getRangeResult.push(i);
-  }
-  return getRangeResult;
-}
-
-function getAverage(start: number, end: number): number {
-  const getAverageResult = (start + end) / 2;
-  return getAverageResult;
-}
-
 function getRangeReport(start: number, end: number): RangeReportObject {
+  const odds = [];
+  const evens = [];
+  const range = [];
+  const average = (start + end) / 2;
+  const total = getTotal(start, end);
+  for (let i = start; i <= end; i++) {
+    range.push(i);
+    if (i % 2 === 0) {
+      evens.push(i);
+    } else {
+      odds.push(i);
+    }
+  }
   const getRangeReportResult = {
-    total: getTotal(start, end),
-    odds: getOdds(start, end),
-    evens: getEvens(start, end),
-    range: getRange(start, end),
-    average: getAverage(start, end),
+    total,
+    odds,
+    evens,
+    range,
+    average,
   };
   return getRangeReportResult;
 }
