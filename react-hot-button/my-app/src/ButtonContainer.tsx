9c65ace63b55acea75711d48099a126e6ac8d313
Skylar Shaffer <lfz@skylarshaffer.com>
2024-05-15 21:07:17 +0000

    Improved react-hot-button challenge.

--- react-hot-button/my-app/src/ButtonContainer.tsx
+++ react-hot-button/my-app/src/ButtonContainer.tsx
@@ -7,35 +7,15 @@ type Props = {
 
 export function ButtonContainer({ buttonText }: Props) {
   const [buttonIndex, setButtonIndex] = useState(0);
-  const [colorIndex, setColorIndex] = useState(0);
-
   function handleClick() {
     setButtonIndex(buttonIndex + 1);
-    switch (buttonIndex + 1) {
-      case 3:
-        setColorIndex(1);
-        break;
-      case 6:
-        setColorIndex(2);
-        break;
-      case 9:
-        setColorIndex(3);
-        break;
-      case 12:
-        setColorIndex(4);
-        break;
-      case 15:
-        setColorIndex(5);
-        break;
-      case 18:
-        setColorIndex(6);
-        break;
-    }
   }
 
   return (
     <div>
-      <button className={'color' + colorIndex.toString()} onClick={handleClick}>
+      <button
+        className={'color' + Math.floor(buttonIndex / 3).toString()}
+        onClick={handleClick}>
         {buttonText}
       </button>
     </div>
