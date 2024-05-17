d2e0ecc58ad7d11ce39895da88282a55dba1c94d
Skylar Shaffer <lfz@skylarshaffer.com>
2024-05-17 16:57:37 +0000

    Completed react-stopwatch challenge.

---
+++ react-stopwatch/my-app/src/Stopwatch.tsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import { FaPlay } from 'react-icons/fa';
+import { FaPause } from 'react-icons/fa';
+
+export function Stopwatch() {
+  const [count, setCount] = useState(0);
+  const [intervalId, setIntervalId] = useState<NodeJS.Timeout>();
+
+  function handlePlayClick() {
+    setIntervalId(
+      setInterval(() => {
+        setCount((prev) => prev + 1);
+      }, 1000)
+    );
+  }
+
+  function handlePauseClick() {
+    clearInterval(intervalId);
+    setIntervalId(undefined);
+  }
+
+  return (
+    <div className="container">
+      <div
+        className="stopwatch"
+        onClick={() => {
+          intervalId || setCount(0);
+        }}>
+        <span>{count}</span>
+      </div>
+      <FaPlay
+        style={{ display: intervalId ? 'none' : '' }}
+        onClick={handlePlayClick}
+      />
+      <FaPause
+        style={{ display: intervalId ? '' : 'none' }}
+        onClick={handlePauseClick}
+      />
+    </div>
+  );
+}
