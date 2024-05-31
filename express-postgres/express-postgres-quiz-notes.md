5df673506e5bc676ba3434d7d006197991bc59d3
Skylar Shaffer <lfz@skylarshaffer.com>
2024-05-31 18:49:36 +0000

    Completed express-postgres exercise.

---
+++ express-postgres/express-postgres-quiz-notes.md
@@ -0,0 +1,61 @@
+# express-postgres-quiz-notes
+
+## Quiz Questions
+
+Answer the following questions in the provided markdown file before turning in this exercise
+
+- What is the purpose of the `pg` NPM package?
+
+  > `pg` allows us to connect a database for express to query using SQL.
+
+- How do you tell `pg` which database to connect to?
+
+  > Pass the database address to the `connectionString` property of the `Pool` object.
+
+- How do you send SQL to PostgreSQL from your Express server?
+
+  > You send SQL to PostgreSQL from your Express server by calling `[databaseObject].query()` with one argument, the SQL query as a string.
+
+- How do you get the rows return from the SQL query?
+
+  > You get the returned rows from the SQL query automatically. You can capture this return by assigning the result of the awaited query to a variable.
+  >
+  > ```TS
+  > const return = await db.query(sql, params)
+  > ```
+
+- What must you always remember to put around your asynchronous route handlers? Why?
+
+  > You must always put the database connection and express application initialization before your asynchronous route handlers so that the routes have the integral objects for their queries. The use method of the application object should follow any specific handlers, and listen should come last.
+
+- What is a SQL Injection Attack and how do you avoid it in `pg`?
+
+  > An SQL Injection Attack is when an attacker places unanticipated SQL code in their query in an attempt to insert or extract unauthorized, sensitive data from a database. You can avoid it in `pg` by using parameterized queries. To do so, you would call `[databaseObject].query()` with **two** arguments instead of one. You would not pass any variables directly into the query string, instead using $ variable notation ($1, $2, ...) in the query string, and passing the parameters as an array as the second argument.
+
+## Notes
+
+All student notes should be written here.
+
+How to write `Code Examples` in markdown
+
+for JS:
+
+```javascript
+const data = 'Howdy';
+```
+
+for HTML:
+
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+
+```css
+div {
+  width: 100%;
+}
+```
---
+++ express-postgres/server.ts
@@ -0,0 +1,101 @@
+import express from 'express';
+import { ClientError, errorMiddleware } from './lib/index.js';
+import pg from 'pg';
+
+// only create ONE pool for your whole server.
+// Note the database name at the end of the connection string.
+const db = new pg.Pool({
+  connectionString: 'postgres://dev:dev@localhost/pagila',
+  ssl: {
+    // Allow non-SSL traffic to localhost
+    rejectUnauthorized: false,
+  },
+});
+
+const app = express();
+
+//  GET /api/films returns the title and filmId of all films in the table, ordered from highest replacement cost to lowest
+
+app.get('/api/films', async (req, res, next) => {
+  try {
+    const sql = `
+      select
+        "filmId",
+        "title",
+        "releaseYear"
+      from "films";
+    `;
+    const result = await db.query(sql);
+    const films = result.rows;
+    if (!films) {
+      throw new ClientError(404, `no films`);
+    }
+    res.send(films);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// GET /api/film returns a single film, based on filmId. Pass filmId as a query parameter. Return a 404 Not Found if the filmId is not in the database.
+
+app.get('/api/film', async (req, res, next) => {
+  try {
+    const { filmId } = req.query;
+    if (filmId === undefined) {
+      throw new ClientError(400, 'filmId is required');
+    }
+    const sql = `
+      select
+        "filmId",
+        "title",
+        "releaseYear"
+      from "films"
+      where "filmId" = $1;
+    `;
+    const params = [filmId];
+    const result = await db.query(sql, params);
+    const film = result.rows[0];
+    if (!film) {
+      throw new ClientError(404, `film ${filmId} not found`);
+    }
+    res.send(film);
+  } catch (err) {
+    next(err);
+  }
+});
+
+//  PUT /api/film updates a single film title, based on filmId. Pass filmId and title as query parameters. Return a success message. Return a 404 Not Found if the filmId is not in the database.
+
+app.put('/api/film', async (req, res, next) => {
+  try {
+    const { filmId, title } = req.query;
+    const missing = [] as string[];
+    filmId || missing.push('filmId');
+    title || missing.push('title');
+    if (missing.length) {
+      throw new ClientError(400, `${missing.join(',')} is required`);
+    }
+    const sql = `
+      update "films"
+      set "title" = $2
+      where "filmId" = $1
+      returning *;
+    `;
+    const params = [filmId, title];
+    const result = await db.query(sql, params);
+    const film = result.rows[0];
+    if (!film) {
+      throw new ClientError(404, `film ${filmId} not found`);
+    }
+    res.status(200);
+    res.send();
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.use(errorMiddleware);
+
+app.listen(8080, () => {
+  console.log('listening on port 8080');
+});
