fe15c41612074f7e02485036e4e6a7fa0ec8f590
Skylar Shaffer <lfz@skylarshaffer.com>
2024-06-04 03:02:21 +0000

    Completed fetch-in-react exercise.

---
+++ fetch-in-react/my-app/src/App.tsx
@@ -0,0 +1,5 @@
+import { Users } from './Users';
+
+export function App() {
+  return <Users />;
+}
---
+++ fetch-in-react/my-app/src/UserDetails.tsx
@@ -0,0 +1,53 @@
+/* eslint-disable @typescript-eslint/no-unused-vars -- Remove me */
+import { useEffect, useState } from 'react';
+import { UserCard } from './UserCard';
+import type { User } from './Users';
+
+type Props = {
+  userId: number;
+  onCancel: () => void;
+};
+export function UserDetails({ userId, onCancel }: Props) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<unknown>();
+  const [user, setUser] = useState<User>();
+
+  useEffect(() => {
+    async function getUser() {
+      try {
+        setUser(
+          await (
+            await fetch(
+              `https://jsonplaceholder.typicode.com/users/${userId}`,
+              {
+                method: 'GET',
+              }
+            )
+          ).json()
+        );
+      } catch {
+        setError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    getUser();
+  }, [userId]);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  if (error || !user) {
+    console.error('Fetch error:', error);
+    return (
+      <p>Error! {error instanceof Error ? error.message : 'Unknown Error'}</p>
+    );
+  }
+
+  return (
+    <div>
+      <button onClick={() => onCancel()}>Close</button>
+      <UserCard user={user} />
+    </div>
+  );
+}
