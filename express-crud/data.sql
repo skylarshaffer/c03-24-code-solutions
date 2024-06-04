11c800fbeed272ae88e1469526571412adcf79be
Skylar Shaffer <161654841+skylarshaffer@users.noreply.github.com>
2024-06-04 08:12:10 -0700

    Revert "express-crud - Skylar Shaffer"

--- express-crud/data.sql
+++
@@ -1,5 +0,0 @@
-insert into "grades" ("name", "course", "score")
-values ('Simon Peyton Jones', 'Haskell', 100),
-       ('Barbara Liskov', 'CLU', 100),
-       ('Rasmus Lerdorf', 'PHP', 100),
-       ('Tim Davis', 'JavaScript', 53);
--- express-crud/lib/client-error.ts
+++
@@ -1,5 +0,0 @@
-export class ClientError extends Error {
-  constructor(public status: number, message: string) {
-    super(message);
-  }
-}
