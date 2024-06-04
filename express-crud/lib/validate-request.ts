7b919b62a17cd04e107b910221607e409fb60128
Skylar Shaffer <lfz@skylarshaffer.com>
2024-06-04 00:31:56 +0000

    Polished express-crud challenge.

--- express-crud/lib/validate-request.ts
+++ express-crud/lib/validate-request.ts
@@ -1,22 +1,42 @@
 import { ClientError } from './client-error';
 
-interface GradeRequestBody extends ReadableStream<Uint8Array> {
-  name: string;
-  course: string;
-  number: number;
-}
+type GradeRequestBody = {
+  name?: string;
+  course?: string;
+  score?: number;
+};
 
-export function ValidateRequestBody(
-  reqBody: GradeRequestBody,
-  reqArray: string[] = ['name', 'course', 'number']
-): void {
+type GradeRequestParams = {
+  gradeId?: string;
+};
+
+type GradeRequest = {
+  body: GradeRequestBody;
+  params: GradeRequestParams;
+};
+
+export function ValidateRequestBody(req: GradeRequest): void {
+  const reqParams = { ...req.body, ...req.params };
   const missing = [] as string[];
-  if (reqBody === null) return;
-  reqArray.forEach((prop) => {
-    Object.prototype.hasOwnProperty.call(reqBody, prop) ||
-      (reqBody[prop] === undefined && missing.push('name'));
-  });
+  for (const param in reqParams) {
+    (param === undefined || param === '') && missing.push(`${param}`);
+  }
   if (missing.length) {
     throw new ClientError(404, `${missing.join(',')} is required`);
+  }
+  if (reqParams.score != null) {
+    if (!Number.isInteger(reqParams.score)) {
+      throw new ClientError(400, `Non-integer score: ${reqParams.score}`);
+    } else if (reqParams.score > 100 || reqParams.score < 0) {
+      throw new ClientError(
+        400,
+        `score is not in the range of 1-100: ${reqParams.score}`
+      );
+    }
+  }
+  if (reqParams.gradeId != null) {
+    if (!Number.isInteger(+reqParams.gradeId)) {
+      throw new ClientError(400, `Non-integer gradeId: ${reqParams.gradeId}`);
+    }
   }
 }
--- express-crud/server.ts
+++ express-crud/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import pg from 'pg';
 import { ClientError, errorMiddleware } from './lib/index.js';
+import { ValidateRequestBody } from './lib/validate-request.js';
 
 const db = new pg.Pool({
   connectionString: 'postgres://dev:dev@localhost/studentGradeTable',
@@ -18,13 +19,7 @@ app.use(express.json());
 app.post('/api/grades', async (req, res, next) => {
   try {
     const { name, course, score } = req.body;
-    const missing = [] as string[];
-    name === undefined && missing.push('name');
-    course === undefined && missing.push('course');
-    score === undefined && missing.push('score');
-    if (missing.length) {
-      throw new ClientError(400, `${missing.join(',')} is required`);
-    }
+    ValidateRequestBody(req);
     const sql = `
       INSERT INTO "grades" ("name","course","score")
       VALUES ($1, $2, $3)
@@ -57,9 +52,7 @@ app.get('/api/grades', async (req, res, next) => {
 app.get('/api/grades/:gradeId', async (req, res, next) => {
   try {
     const { gradeId } = req.params;
-    if (!Number.isInteger(+gradeId)) {
-      throw new ClientError(400, `Non-integer gradeId: ${gradeId}`);
-    }
+    ValidateRequestBody(req);
     const sql = `
       SELECT * FROM "grades"
       WHERE "gradeId" = $1;
@@ -79,25 +72,7 @@ app.put('/api/grades/:gradeId', async (req, res, next) => {
   try {
     const { gradeId } = req.params;
     const { name, course, score } = req.body;
-    const missing = [] as string[];
-    name === undefined && missing.push('name');
-    course === undefined && missing.push('course');
-    score === undefined && missing.push('score');
-    if (missing.length) {
-      throw new ClientError(400, `${missing.join(',')} is required`);
-    }
-    if (!Number.isInteger(+gradeId)) {
-      throw new ClientError(400, `Non-integer gradeId: ${gradeId}`);
-    }
-    if (score > 100 || score < 0) {
-      if (!Number.isInteger(+score)) {
-        throw new ClientError(400, `Non-integer score: ${score}`);
-      }
-      throw new ClientError(
-        400,
-        `score is not in the range of 1-100: ${score}`
-      );
-    }
+    ValidateRequestBody(req);
     const sql = `
       UPDATE "grades"
       SET "name" = $2, "course" = $3, "score" = $4
@@ -118,9 +93,7 @@ app.put('/api/grades/:gradeId', async (req, res, next) => {
 app.delete('/api/grades/:gradeId', async (req, res, next) => {
   try {
     const { gradeId } = req.params;
-    if (!Number.isInteger(+gradeId)) {
-      throw new ClientError(400, `Non-integer gradeId: ${gradeId}`);
-    }
+    ValidateRequestBody(req);
     const sql = `
       DELETE FROM "grades"
       WHERE "gradeId" = $1
